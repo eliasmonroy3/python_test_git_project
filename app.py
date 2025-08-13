59579bb655ea37fc78cd34d122455c8b68c22625
Elias Monroy <140570029+eliasmonroy3@users.noreply.github.com>
2025-08-12 22:20:05 -0600

    se agregó nueva funcion nueva rama y se  agrego funcion resta

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 
 def suma(num1, num2):
 	return num1 + num2
+	
+def resta(num1, num2):
+	return num1 - num2
 
 print("¡Hola Git! Este es mi primer commit.")
 
 print(f"\n Este es el resultado de la suma {suma(5, 10)}")
+
+print(f"\n Este es el resultado de la resta {resta(50, 10)}")
