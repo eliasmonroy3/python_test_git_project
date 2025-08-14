cbc9007b55d19bdb389c94a3dc15df8686b22044
Elias Monroy <140570029+eliasmonroy3@users.noreply.github.com>
2025-08-13 20:55:37 -0600

    se modifico el archivo app.py usando autopep8

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 
 def suma(num1, num2):
-	return num1 + num2
-	
+    return num1 + num2
+
+
 def resta(num1, num2):
-	return num1 - num2
+    return num1 - num2
+
 
 print("¡Hola Git! Este es mi primer commit.")
 
